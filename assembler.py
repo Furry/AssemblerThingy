4741ba4bfcc98362b782e2569ba0889cc4d66fa8
A Furry Account <ender@thefluegges.com>
2021-04-01 11:48:44 -0400

    Put a condom over the cod

--- assembler.py
+++ assembler.py
@@ -4,14 +4,18 @@ assem_code = []
 des_file = input("what file do you want to load?\n")
 cur_file = open(des_file, "r")
 
+# Iterate over all ines
 for line in cur_file:
-	values = line.split()
-	if len(values) == 0:
-		com_count += 1
-	elif values[0] == "//":
-		com_count += 1
-	else:
-		assem_code.append(values[0])
-		
+    # Split by new line
+    values = line.split()
+    if len(values) == 0:
+        com_count += 1
+    # RM comment
+    elif values[0] == "//":
+        com_count += 1
+    else:
+        assem_code.append(values[0])
+map(lambda x: "//".split(x)[0], assem_code)
+
 for i in assem_code:
-	print(i)
+    print(i)
